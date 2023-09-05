c2ef15f59835cf0f5306a91d7d9eabfea71c9e81
janaki rao <kalagajanakirao@gmail.com>
2023-09-05 17:03:06 +0530

    JsVarablesAndDataTypes

---
+++ JsVarablesAndDataTypes.js
@@ -0,0 +1,80 @@
+/*----------------------By Using  variables var --------------------------------------
+
+// Multiple-lines -------------------------------------->way 01
+    var x;   //declaration
+    x=100;   //intialization
+
+//single Statement declaration&intialistion ------------>way 02
+    var x=100;
+
+//single line - Multiple Statements -------------------->way 03
+    var x=300,y=400;
+
+//print 2ways
+    document.write(x);
+    console.log(x);
+*/                             
+
+/* --------------------  By using Let variables  ----------------------------------------
+
+  // multiple variables --------------------------------->01
+     let x;
+     x=100;
+
+ //single Statement declaration&intialistion ------------>way 02  
+     let y=200;
+
+  //single line - Multiple Statements -------------------->way 03     
+
+    let a=300,b=400;
+
+*/
+
+//---------------------------  By using const variables ---------------------------------
+   
+/* *************** df b/w ---->Let & Var<----- *************
+  1) In Var variable can assign Any Ever It will Excutess in -----var----- Anywhere
+     console.log(x);
+      var x =100;
+     console.log(x);
+
+  2) In Var variable can assign Any Ever It will Excutess in -----Let----- only This way
+     var x =100;
+     console.log(x);
+
+  3) In Var variable can assign Any Ever It will not Excutess bzc const takes constant value only -----const----- 
+      x =400;           
+      console.log(x);
+      var x =100;               //if in case this is not here it will excutes
+      console.log(x);
+
+*/     
+
+
+/*-------------------------------------------DataTypes------------------------------------------
+
+    ------if yoy Don't know what is the datatype we can use ---------(typeof method)-----------
+
+     let x=100;                  //------------------Number Method---->01
+     console.log(typeof(x));
+
+     let price =100.50;          //------------------float Method----->02
+     console.log(typeof(price));
+
+     let stuname="John"          //------------------String Method---->03
+     console.log(typeof(stuname));
+
+     let flag= flase;            //------------------boolean Method--->04
+     console.log(typeof(flag));
+
+     let status=null;            //------------------object Method---->05
+     console.log(typeof(status));
+
+     let value=undefined;        //------------------Undefined Method-->06
+     console.log(typeof(value));
+
+*/     
+
+document.write("<h1> jaan</h>")
+
+
